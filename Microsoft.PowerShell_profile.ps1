2b837be9f59857c6123009fde53ed8782159c6ee
Piotr Kudrel <piotr.kudrel@deneblab.com>
2017-08-22 10:19:57 -0400

    Init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿
+# Load posh-git example profile
+. 'C:\Users\piotr\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+# Utf8
+# https://stackoverflow.com/questions/40098771/changing-powershells-default-output-encoding-to-utf-8
+
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
